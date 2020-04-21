92adb9c23e1a526f43785fb03e0e5eb604eafdaa
vcnsiqueira <35041704+vcnsiqueira@users.noreply.github.com>
2020-04-20 21:27:49 -0300

    Criação do arquivo post.html para envio de requisições para o servidor. Para isto, foi necessário implementar o método post no HttpService.js

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -23,4 +23,28 @@ class HttpService {
         });
     }
 
+    post(url, dado) {
+
+        return new Promise((resolve, reject) => {
+
+            let xhr = new XMLHttpRequest(); // criando uma instância de XMLHttpRequest
+
+            xhr.open('POST', url, true)
+
+            xhr.setRequestHeader("Content-Type", "application/json"); // adicionando ao cabeçalho da requisição qual tipo de conteúdo estamos enviando (JSON)
+            xhr.onreadystatechange = () => {
+                if(xhr.readyState == 4) {
+                    if(xhr.status == 200) {
+                        resolve(JSON.parse(xhr.responseText));
+                    } else {
+                        reject(xhr.responseText);
+                    }
+                }
+            };
+            xhr.send(JSON.stringify(dado));
+
+        });
+
+    }
+
 }
